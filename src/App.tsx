47ecfd207e3a3c5343c5d9053454f9291da84e24
BlackBerryID <iddf1994one@gmail.com>
2022-06-12 16:43:13 +0300

    Init: add basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+export const App = () => {
+  // const data = {
+  //   prop: 'value',
+  // };
 
-function App() {
+  // const writeData = async (data: { prop: string }) => {
+  //   await fetch('./data.json', {
+  //     headers: {
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json',
+  //     },
+  //     method: 'POST',
+  //     body: JSON.stringify(data),
+  //   });
+  // };
+
+  // const readData = async () => {
+  //   const response = await fetch('./data.json');
+  //   const data = await response.json();
+  //   console.log(data);
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <button onClick={() => readData()}>READ</button>
+      <button onClick={() => writeData(data)}>WRITE</button> */}
+    </>
   );
-}
-
-export default App;
+};
